b77c632a75d5f4ddf17c4eb3b5f1b898d85f78a5
Aleksey Veselovskiy <ballarnes@gmail.com>
2021-10-31 01:38:43 +0300

    completed homework

--- Module3HW8/Module3HW8/Program.cs
+++ Module3HW8/Module3HW8/Program.cs
@@ -1,11 +1,11 @@
-﻿using System;
-
-namespace Module3HW8
+﻿namespace Module3HW8
 {
     public class Program
     {
         private static void Main(string[] args)
         {
+            var appSet = new ApplicationSet();
+            appSet.Setup();
         }
     }
 }
