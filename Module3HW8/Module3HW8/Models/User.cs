aea4f897f120cc052a2be7b24e4aabc41e166940
Aleksey Veselovskiy <ballarnes@gmail.com>
2021-10-30 19:28:58 +0300

    add additional files

---
+++ Module3HW8/Module3HW8/Models/User.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Module3HW8.Models
+{
+    public class User
+    {
+    }
+}
